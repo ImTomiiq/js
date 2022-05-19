06d500adc73f9fa5e89c9fa84efad3ad21ee5299
Tomáš Sucháň <tomiiq16@gmail.com>
2022-05-19 21:20:28 +0200

    516

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,17 @@
+/* const country = "Slovensko";
+const continent = "Európa";
+let population = "5 Miliónov";
+const isIsland = false;
+const language = "Slovenčina";
+
+console.log(
+  `Krajina: ${country}, Jazyk ${language}, Kontinent ${continent} a Populácia ${population} Ostrovná krajina? ${isIsland}`
+); */
+
+let kokot = true;
+
+console.log(typeof kokot);
+console.log(typeof 23);
+
+kokot = "ANOOOO";
+console.log(typeof kokot);
