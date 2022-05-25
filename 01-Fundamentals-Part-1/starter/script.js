50fe02b5b84b9b9180a82d7c35755cbfd84bf9a0
Tomáš Sucháň <tomiiq16@gmail.com>
2022-05-25 10:00:46 +0200

    d

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -20,3 +20,24 @@ const johnBMI = console.log(johnMass / (johnHeight * johnHeight));
 
 let markHigherBMI = true;
 console.log(markHigherBMI); */
+
+/*let markMass = 78;
+let johnMass = 92;
+
+let markHeight = 1.69;
+let johnHeight = 1.95;
+
+const markBMI = markMass / (markHeight * markHeight);
+console.log(markBMI);
+const johnBMI = johnMass / (johnHeight * johnHeight);
+console.log(johnBMI);
+
+if (markBMI > johnBMI) {
+  console.log(`Mark has more BMI ${markBMI}`);
+} else {
+  console.log(`John has more BMI ${johnBMI}`);
+}
+*/
+
+const inputYear = "1991";
+console.log(inputYear + 18);
