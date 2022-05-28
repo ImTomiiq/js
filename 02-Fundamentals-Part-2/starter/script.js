3d2af9e89f2b91fe4a64a143be269fee5abc7456
Tomáš Sucháň <tomiiq16@gmail.com>
2022-05-28 14:35:54 +0200

    switch statement

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -306,7 +306,7 @@ const calcAverage = function (arr) {
 
 console.log(calcAverage([1, 5, 3])); */
 
-let lang = "English";
+/*let lang = "English";
 let population = 50000000;
 let isIsland = false;
 
@@ -314,4 +314,29 @@ if (lang === "English" && population === 50000000 && isIsland !== true) {
   console.log("You should live in Portugal");
 } else {
   console.log("Portugal does not meet your criteria");
+} */
+
+const language = "chinese";
+
+switch (language) {
+  case "chinese":
+  case "mandarin":
+    console.log("Most number of native speakers!");
+    break;
+  case "spanish":
+    console.log("2nd place in number of native speakers");
+    break;
+
+  case "english":
+    console.log("3rd place");
+    break;
+  case "hindi":
+    console.log("Number 4");
+    break;
+  case "arabic":
+    console.log("5th most spoken language");
+    break;
+  default:
+    console.log("Great language too :D");
+    break;
 }
