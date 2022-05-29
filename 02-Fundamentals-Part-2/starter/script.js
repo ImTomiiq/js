057561061f66508e2e27de5666b583c08909b366
Tomáš Sucháň <tomiiq16@gmail.com>
2022-05-29 22:38:42 +0200

    dsa

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -363,4 +363,4 @@ const describePopulation = function (country, population) {
 describePopulation("China", 1441);
 */
 
-const populations = [150, 1250, 35871, 40000];
+const populatdsaions = [150, 1250, 35871, 40000];
